fe031fe2b95918622b2429b11cadac2167690b89
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-08 11:04:06 -0700

    reverts to local requests

--- camera.js
+++ camera.js
@@ -3,12 +3,12 @@ const app = express();
 const router = express.Router();
 const os = require('os');
 const path = require('path');
-const port = 42222;
+const port = 8888;
 const cors = require('cors');
 const leds = require('./api/leds');
 const camera = require('./api/camera');
 
-app.use(cors({origin: '*'}));
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use('/leds', leds);
 app.use('/camera', camera);
 
