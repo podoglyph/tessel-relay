96baa70efb094bbebc66f5c0b2d412dab7e1cdeb
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-07 22:30:34 -0700

    adds new port for access through public ip

--- camera.js
+++ camera.js
@@ -3,12 +3,12 @@ const app = express();
 const router = express.Router();
 const os = require('os');
 const path = require('path');
-const port = 8888;
+const port = 42222;
 const cors = require('cors');
 const leds = require('./api/leds');
 const camera = require('./api/camera');
 
-app.use(cors({origin: 'http://localhost:8080'}));
+app.use(cors({origin: '*'}));
 app.use('/leds', leds);
 app.use('/camera', camera);
 
